2fcc26477bca667c4ecf1c2520064f3ee2c3dc6c
sviatoslavkovalchuk <sviatoslav.kovalchuk97@gmail.com>
2023-12-26 12:04:46 +0200

    Updated test structure for jenkins

--- test_ukr_net.py
+++ test_ukr_net.py
@@ -1,9 +1,21 @@
 
-def test_simple_assert_ukr_net(login_to_ukr_net_via_imap, email_parser):
+def test_asert_data_subject(login_to_ukr_net_via_imap, email_parser):
     data = email_parser
     assert data['Subject'] == 'Test'
+
+
+def test_asert_email(login_to_ukr_net_via_imap, email_parser):
+    data = email_parser
     assert 'sviatoslav.kovalchuk97@gmail.com' in data['From']
+
+
+def test_asert_data_to(login_to_ukr_net_via_imap, email_parser):
+    data = email_parser
     assert data['To'] == 'sviatoslavio@ukr.net'
+
+
+def test_expected_text_in_email_body(login_to_ukr_net_via_imap, email_parser):
+    data = email_parser
     expected_text = 'Test Assert Python'
     for part in data.walk():
         if part.get_content_type() == 'text/plain':
